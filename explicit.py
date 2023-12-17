884aaaa0aa9661c1cbc382026b8e12b742424248
femailman <93400575+femailman@users.noreply.github.com>
2023-12-17 13:10:41 -0500

    Added login page, credentials, and get liked songs page

--- explicit.py
+++ explicit.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests, time, os
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from dotenv import load_dotenv
 
@@ -16,26 +18,68 @@ load_dotenv()
 SPOTIFY_EMAIL = os.getenv("SPOTIFY-EMAIL")
 SPOTIFY_PASSWORD = os.getenv("SPOTIFY-PASSWORD")
 
-print(SPOTIFY_EMAIL)
-print(SPOTIFY_PASSWORD)
-
 service = ChromeService(executable_path=CHROME_DRIVER_PATH)
 driver = webdriver.Chrome(service=service, options=options)
-driver.get(SPOTIFY)
 
 
 def get_login_page():
-    print("done sleep")
-    login_button = driver.find_element(
-        By.CLASS_NAME,
-        "fyugtm",
-    )
-    print("loginbutton", login_button)
-    login_button.click()
+    """
+    Enters login page from spotify main page
+    """
+    try:
+        login_button = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//*[@id='main']/div/div[2]/div[3]/header/div[4]/div[2]/button[2]",
+                )
+            )
+        )
+        login_button.click()
+    except:
+        driver.quit()
+
+
+def enter_credentials():
+    """
+    Enters email and password anad click login button
+    """
+    try:
+        email_text_box = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.ID, "login-username"))
+        )
+        email_text_box.send_keys(SPOTIFY_EMAIL)
+        password_text_box = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.ID, "login-password"))
+        )
+        password_text_box.send_keys(SPOTIFY_PASSWORD)
+
+        login_button = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable((By.ID, "login-button"))
+        )
+        login_button.click()
+    except:
+        driver.quit()
+
+
+def get_liked_page():
+    try:
+        buttons = WebDriverWait(driver, 3).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "hIehTT"))
+        )
+        liked_songs_button = buttons[0]
+        liked_songs_button.click()
+    except:
+        driver.quit()
 
 
 def main():
+    driver.get(SPOTIFY)
     get_login_page()
+    enter_credentials()
+    get_liked_page()
+    time.sleep(5)
+    driver.quit()
 
 
 if __name__ == "__main__":
