9a6eeba1b32d1f5bff1cfc62cb551e8d00826961
femailman <93400575+femailman@users.noreply.github.com>
2023-12-17 15:28:58 -0500

    Change get_liked_song to return a song based on index

--- explicit.py
+++ explicit.py
@@ -87,61 +87,46 @@ search song title and song
 """
 
 
-def get_liked_songs():
+def get_liked_song(index: int):
     try:
-        body = driver.find_element(
+        song = driver.find_element(
             By.XPATH,
-            "//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div[4]/div",
+            f'//*[@id="main"]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div[4]/div/div[2]/div[2]/div[{index + 1}]/div',
         )
-        songs = []
-        scroll_count = 15  # Adjust the number of scrolls as needed
-        for i in range(scroll_count):
-            body.send_keys(Keys.ARROW_DOWN)
-            song = driver.find_element(
-                By.XPATH,
-                f'//*[@id="main"]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div[4]/div/div[2]/div[2]/div[{i + 1}]/div',
-            )
-            song_title = song.find_element(By.XPATH, "./div[2]/div/a/div").text
-            print("title", song_title)
-            try:
-                explicit_span = song.find_element(By.XPATH, "./div[2]/div/span[1]/span")
-                print("in try", explicit_span.text)
-                if "E" in explicit_span.text:
-                    song_explicit = True
-                else:
-                    song_explicit = False
-                song_artist = song.find_element(
-                    By.XPATH, "./div[2]/div/span[2]/div/a"
-                ).text
-            except:
-                song_explicit = False
-                song_artist = song.find_element(
-                    By.XPATH, "./div[2]/div/span/div/a"
-                ).text
-            print("expl", song_explicit)
-            print("artist", song_artist)
-            song_album = song.find_element(By.XPATH, "./div[3]/span/span/a").text
-            print("alb", song_album)
-            song_mins, song_seconds = song.find_element(
-                By.XPATH, "./div[5]/div"
-            ).text.split(":")
-            print("mins", song_mins)
-            print("secs", song_seconds)
-            print("**********")
-            song = Song(
-                title=song_title,
-                artist=song_artist,
-                song_album=song_album,
-                explicit=song_explicit,
-                mins=song_mins,
-                seconds=song_seconds,
-                date_added=None,
-            )
-
-            songs.append(song)
-        print("songs", songs, "length of songs", len(songs), "done")
+        song_title = song.find_element(By.XPATH, "./div[2]/div/a/div").text
+        try:
+            song.find_element(By.XPATH, "./div[2]/div/span[1]/span")
+            song_explicit = True
+            song_artist = song.find_element(By.XPATH, "./div[2]/div/span[2]/div/a").text
+        except:
+            song_explicit = False
+            song_artist = song.find_element(By.XPATH, "./div[2]/div/span/div/a").text
+        song_album = song.find_element(By.XPATH, "./div[3]/span/span/a").text
+        song_mins, song_seconds = song.find_element(
+            By.XPATH, "./div[5]/div"
+        ).text.split(":")
+        song_mins = int(song_mins)
+        song_seconds = int(song_seconds)
+        song = Song(
+            title=song_title,
+            artist=song_artist,
+            album=song_album,
+            explicit=song_explicit,
+            mins=song_mins,
+            seconds=song_seconds,
+        )
+        return song
     except:
         driver.quit()
+
+
+# body = driver.find_element(
+#     By.XPATH,
+#     "//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/section/div[4]/div",
+# )
+# songs = []
+# scroll_count = 15  # Adjust the number of scrolls as needed
+# body.send_keys(Keys.ARROW_DOWN)
 
 
 def main():
@@ -152,7 +137,7 @@ def main():
     enter_credentials()
     get_liked_page()
     time.sleep(4)
-    get_liked_songs()
+    print(get_liked_song(2))
 
 
 if __name__ == "__main__":
